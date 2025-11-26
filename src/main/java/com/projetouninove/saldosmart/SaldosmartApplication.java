32a31dd6140d1801050b7eee4112a71600904dbc
Diego Duarte <diegocavalcante040307@gmail.com>
2025-11-25 22:03:00 -0300

    commit incial

---
+++ src/main/java/com/projetouninove/saldosmart/SaldosmartApplication.java
@@ -0,0 +1,13 @@
+package com.projetouninove.saldosmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaldosmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaldosmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetouninove/saldosmart/SaldosmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetouninove.saldosmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaldosmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
