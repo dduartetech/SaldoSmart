3d234b24baff437591b25a651492a7bf0646ddbf
Diego Duarte <diegocavalcante040307@gmail.com>
2025-11-30 14:28:44 -0300

    reparamento final

--- src/main/java/com/projetouninove/saldosmart/SaldosmartApplication.java
+++ src/main/java/com/projetouninove/saldosmart/SaldosmartApplication.java
@@ -2,8 +2,10 @@ package com.projetouninove.saldosmart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SaldosmartApplication {
 
 	public static void main(String[] args) {
